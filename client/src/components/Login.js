01ffb246c3ffdcd1025372a0974095bc7d179b22
BackinR2D2 <cristimlg334@gmail.com>
2020-10-05 20:09:42 +0300

    initialize login component / initialize login route

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
+import * as yup from 'yup';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -12,19 +14,62 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+const schema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().max(256).required(),
+})
+
 function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const classes = useStyles();
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleLogin = () => {
+        schema.isValid({
+            email: email,
+            password: password
+        })
+            .then((resp) => {
+                console.log(resp);
+                if (!resp) {
+                    return;
+                } else {
+                    axios.post('/login', { email, password })
+                        .then((data) => {
+                            console.log(data);
+                        })
+                        .catch((err) => {
+                            // TODO: HANDLE ERROR
+                            console.log(err);
+                        })
+                }
+            })
+            .catch((err) => {
+                // TODO: HANDLE ERROR
+                console.log(err);
+            })
+    }
+
     return (
         <div>
             <form className={classes.root}>
                 <div>
-                    <TextField id="outlined-basic" label="Email" variant="outlined" name="email" type="email" required />
+                    <TextField id="outlined-basic" label="Email" variant="outlined" name="email" type="email" onChange={handleEmail} required />
                 </div>
                 <div>
-                    <TextField id="outlined-basic" label="Password" variant="outlined" name="password" type="password" required />
+                    <TextField id="outlined-basic" label="Password" variant="outlined" name="password" type="password" onChange={handlePassword} required />
                 </div>
                 <div>
-                    <Button variant="outlined" color="primary">
+                    <Button variant="outlined" color="primary" onClick={handleLogin}>
                         Login
                     </Button>
                 </div>
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const home = require('./routes/home');
+const login = require('./routes/login');
 const mongoose = require('mongoose');
 
 // middlewares
@@ -23,6 +24,7 @@ mongoose.connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: t
 
 // routes
 app.use(home);
+app.use('/login', login);
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
