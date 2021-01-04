5d52624d78618aeae84789f832d3b0246c6125a5
BackinR2D2 <cristimlg334@gmail.com>
2021-01-04 18:55:11 +0200

    fixing deployment bug

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -199,7 +199,7 @@ function Account() {
                     <hr />
                     <br />
                     {
-                        posts && posts.length === 0 ?
+                        (posts && posts.length === 0) || posts === undefined ?
                             <></>
                             :
                             <div className="buttonSection">
