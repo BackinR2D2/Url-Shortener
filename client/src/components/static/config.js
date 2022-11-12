f851d2a615240e005188ea4d049f1ad02c3ff273
Cristian Niculae <niculaec159@gmail.com>
2022-11-12 15:40:56 +0200

    backend url change

--- client/src/components/static/config.js
+++ client/src/components/static/config.js
@@ -1,5 +1,8 @@
 const config = {
-    URL: process.env.NODE_ENV === 'production' ? 'https://url-shortener-ra.herokuapp.com' : 'http://localhost:5000'
-}
+	URL:
+		process.env.NODE_ENV === 'production'
+			? 'https://short-url-olu1.onrender.com'
+			: 'http://localhost:5000',
+};
 
 export default config;
